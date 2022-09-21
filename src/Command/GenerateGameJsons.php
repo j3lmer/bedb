7bd0ac40a4b44d3fba49b4bb51cee50d23652f82
j3lmer <jelmer339@gmail.com>
2022-09-21 15:00:36 +0200

    Get updated steam games list before populating jsons

--- src/Command/GenerateGameJsons.php
+++ src/Command/GenerateGameJsons.php
@@ -11,10 +11,9 @@ use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Filesystem\Filesystem;
 
-// the name of the command is what users type after "php bin/console"
 #[AsCommand(
     name: 'app:generate-game-jsons',
-    description: 'Creates files with steam appids as name with the respective appdetails in them in json form.',
+    description: 'Gets updated game list from steam and creates files with steam appids as name with the respective appdetails in them in json form.',
     aliases: ['app:gen-games'],
     hidden: false
 )]
@@ -26,6 +25,8 @@ class GenerateGameJsons extends Command
         $filesystem = new Filesystem();
         $package = new Package(new EmptyVersionStrategy());
 
+        $this->getUpdatedGameList($filesystem, $package);
+
         $json = json_decode(
             file_get_contents($package->getUrl('assets/steam/steamgames.json')),
             true
@@ -35,6 +36,14 @@ class GenerateGameJsons extends Command
         $this->dumpGames($filesystem, $package, $gameList);
 
         return Command::SUCCESS;
+    }
+
+    protected function getUpdatedGameList(Filesystem $filesystem, Package $package)
+    {
+        $path = "{$package->getUrl("assets/steam/")}steamgames.json";
+        $response = file_get_contents("https://api.steampowered.com/ISteamApps/GetAppList/v0002/?key=STEAMKEY&format=json");
+        $filesystem->dumpFile($path, $response);
+        printf("Got updated games list!");
     }
 
     protected function dumpGames(Filesystem $filesystem, Package $package, mixed $gameList)
