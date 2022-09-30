45f94e5c378752f02398849133a1d33010ec7283
j3lmer <jelmer339@gmail.com>
2022-09-30 13:57:32 +0200

    formatting

--- assets/vue/pages/entry/components/RegisterForm.vue
+++ assets/vue/pages/entry/components/RegisterForm.vue
@@ -1,5 +1,5 @@
 <template>
-	<v-container>
+    <v-container>
         <v-row>
             <v-spacer/>
             <v-col cols="3">
@@ -50,22 +50,22 @@
             </v-col>
             <v-spacer/>
         </v-row>
-		<v-snackbar
-			v-model="snackbar"
-		>
-			{{ snackbarText }}
-			<template v-slot:action="{ attrs }">
-				<v-btn
-					color="red"
-					text
-					v-bind="attrs"
-					@click="snackbar = false"
-				>
-					Close
-				</v-btn>
-			</template>
-		</v-snackbar>
-	</v-container>
+        <v-snackbar
+            v-model="snackbar"
+        >
+            {{ snackbarText }}
+            <template v-slot:action="{ attrs }">
+                <v-btn
+                    color="red"
+                    text
+                    v-bind="attrs"
+                    @click="snackbar = false"
+                >
+                    Close
+                </v-btn>
+            </template>
+        </v-snackbar>
+    </v-container>
 </template>
 
 <script lang="ts">
@@ -74,49 +74,49 @@ import axios from "axios";
 
 @Component
 export default class RegisterForm extends VueComponent {
-	private valid = true;
-	private snackbar = false;
-	private username = '';
-	private password = '';
+    private valid = true;
+    private snackbar = false;
+    private username = '';
+    private password = '';
     private email = '';
     private passwordCheck = '';
-	private snackbarText = '';
+    private snackbarText = '';
 
-	private nameRules = [
-		v => !!v || 'Username is required',
-		v => (v && v.length <= 254) || 'Username is too long',
-	];
-	private emailRules = [
-		v => !!v || 'E-mail is required',
-		v => /.+@.+\..+/.test(v) || 'E-mail is not valid',
-	];
+    private nameRules = [
+        v => !!v || 'Username is required',
+        v => (v && v.length <= 254) || 'Username is too long',
+    ];
+    private emailRules = [
+        v => !!v || 'E-mail is required',
+        v => /.+@.+\..+/.test(v) || 'E-mail is not valid',
+    ];
 
-	private required(value): boolean | string {
-		return value ? true : 'Password is required';
-	}
+    private required(value): boolean | string {
+        return value ? true : 'Password is required';
+    }
 
-	private min6(value): boolean | string {
-		return value.length >= 6 ? true : 'Password must be at least 6 characters long';
-	}
+    private min6(value): boolean | string {
+        return value.length >= 6 ? true : 'Password must be at least 6 characters long';
+    }
 
-	private matchingPasswords(): boolean | string {
-		return this.password === this.passwordCheck ? true : 'Passwords do not match';
-	}
+    private matchingPasswords(): boolean | string {
+        return this.password === this.passwordCheck ? true : 'Passwords do not match';
+    }
 
-	private async submit(): Promise<void> {
-		if ((this as any).$refs.form.validate() && this.valid) {
-			const user = {
-				username: this.username,
-				email: this.email,
-				plainPassword: this.password
-			}
+    private async submit(): Promise<void> {
+        if ((this as any).$refs.form.validate() && this.valid) {
+            const user = {
+                username: this.username,
+                email: this.email,
+                plainPassword: this.password
+            }
 
             axios
                 .post('/register', user)
                 .then(response => {
                     console.log(response.data);
                     console.log(response.headers);
-                    if(response.status < 299){
+                    if (response.status < 299) {
                         location.replace('/');
                     }
                 })
@@ -128,8 +128,8 @@ export default class RegisterForm extends VueComponent {
                     }
                     this.snackbar = true;
                 })
-		}
-	}
+        }
+    }
 };
 </script>
 
