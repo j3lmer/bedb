64a100e2e057e63274d2eebb1e695f29388f1546
j3lmer <jelmer339@gmail.com>
2022-10-01 16:21:24 +0200

    add vuecookies

--- assets/vue/pages/home/app.js
+++ assets/vue/pages/home/app.js
@@ -2,11 +2,13 @@ import 'core-js/stable'
 import 'regenerator-runtime/runtime'
 import Vue from 'vue';
 import Vuetify from 'vuetify';
+import VueCookies from 'vue-cookies';
 import App from './App.vue';
 import 'vuetify/dist/vuetify.min.css'
 
 Vue.config.productionTip = false
 Vue.use(Vuetify);
+Vue.use(VueCookies);
 
 // eslint-disable-next-line no-new
 const app = new Vue({
