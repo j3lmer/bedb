a5752fc0f13de487258ee82ed6a09d0d6e4637c5
j3lmer <jelmer339@gmail.com>
2022-10-05 13:48:03 +0200

    rename steam repo

--- src/Repository/SteamReviewRepository.php
+++ src/Repository/SteamReviewRepository.php
@@ -14,7 +14,7 @@ use Doctrine\Persistence\ManagerRegistry;
  * @method TeamReview[]    findAll()
  * @method TeamReview[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
  */
-class TeamReviewRepository extends ServiceEntityRepository
+class SteamReviewRepository extends ServiceEntityRepository
 {
     public function __construct(ManagerRegistry $registry)
     {
