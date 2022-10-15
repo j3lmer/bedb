b2cb51c0bc62758bab461b5d703f0c7a096aa952
j3lmer <jelmer339@gmail.com>
2022-10-15 14:47:01 +0200

    successful query for multiple genres at one

--- assets/vue/pages/home/App.vue
+++ assets/vue/pages/home/App.vue
@@ -9,12 +9,16 @@
             <Home   v-if="selectedComponent === items.Home"
                     @updated-tab="selectComponent"
             />
-            <Games  v-if="selectedComponent === items.Games"/>
+            <Games  v-if="selectedComponent === items.Games"
+                    :genre-games="genreGames"
+            />
         </v-card>
     </v-app>
 </template>
 
 <script lang="ts">
+import axios from "axios";
+
 const {Component, VueComponent} = require('@/common/VueComponent');
 import {HomepageTabs} from "@/common/components/Enums/HomepageTabs";
 import Header from "@/pages/home/components/Header.vue";
@@ -32,13 +36,29 @@ export default class App extends VueComponent {
     private user = null;
     private selectedComponent = 'Home';
     private items = HomepageTabs;
+    private genreGames = {};
+    private genreGamesQuery = `query GetGenreWithGamesAndDescription($id: ID!) {
+        genre(id: $id) {
+            description
+            games(first:6) {
+                edges {
+                    node {
+                        id
+                        name
+                        headerImage
+                    }
+                }
+            }
+        }
+    }`;
+
+    private beforeMount(): void {
+        this.loadGames();
+    }
 
     public created(): void {
         let cookiePage = this.$cookies.get('page');
-
-        if ((window as any).user !== null) {
-            this.user = (window as any).user;
-        }
+        this.user = (window as any).user;
 
         if (cookiePage) {
             this.selectComponent(cookiePage);
@@ -47,6 +67,68 @@ export default class App extends VueComponent {
 
     private selectComponent(component: any): void {
         this.selectedComponent = component;
+    }
+
+    private async loadGames(): Promise<void> {
+
+        //append aan het einde nog een }
+        //$id: ID!) {
+        let outerString = `query GetGenreWithGamesAndDescription(`;
+        let innerString = ``;
+        let variables = {};
+
+        for (let i = 0; i < 5; i++) {
+            outerString += i === 4 ? `$id${i}: ID!` : `$id${i}: ID!, `;
+            const randNumber = this.randNumber(1, 13);
+            let genreString = `genre${i} : genre(id: $id${i}) {
+                description
+                games(first:6) {
+                    edges {
+                        node {
+                            id
+                            name
+                            headerImage
+                        }
+                    }
+                }
+            }`
+
+            if(i !== 4) {
+                genreString+= ',';
+            }
+
+            variables[`id${i}`] = `/api/genres/${randNumber}`;
+            innerString += genreString;
+        }
+        outerString += ") {";
+        innerString += '}';
+
+        outerString += innerString;
+        this.genreGames = await this.queryPoster(outerString, variables);
+    }
+
+    private async queryPoster(query: string, variables: object)
+    {
+        console.log(query);
+        console.log(variables);
+
+        const response = await axios.post("http://127.0.0.1:8000/api/graphql", {
+                query: query,
+                variables: variables,
+            },
+            {
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            }
+        );
+
+        return response.data.data
+    }
+
+    private randNumber(min: number, max: number): number
+    {
+        return Math.floor(Math.random()*(max - min + 1) + min)
     }
 }
 </script>
--- assets/vue/pages/home/components/Games.vue
+++ assets/vue/pages/home/components/Games.vue
@@ -1,39 +1,35 @@
 <template>
     <div class="my-8">
-        {{genreGames}}
-
         <div
-            v-for="(category, genre) in categoryGames"
-            :key="genre"
+            v-for="(genre, key) in genreGames"
+            :key="key"
         >
             <v-divider class="my-10" v-if="genre !== 'Featured'"/>
             <v-row align="center" justify="center">
                 <v-spacer/>
                 <v-col>
-                    <h1>{{ genre }}</h1>
+                    <h1>{{ genre.description }}</h1>
                 </v-col>
                 <v-spacer/>
                 <v-spacer/>
             </v-row>
             <v-row class="mx-4">
+
                 <v-carousel
                     cycle
                     hide-delimiters
                     show-arrows-on-hover
                     height="300"
                 >
-                    <v-carousel-item
-                        v-for="(chunk, i) in category"
-                        :key="i"
-                        justify="center"
-                    >
+                    <v-carousel-item justify="center">
                         <v-row
                             align="center"
                             justify="center"
                         >
+<!--                            width van het scherm meenemen in het bepalen van hoe het word gerendered?-->
                             <v-col
-                                v-for="(game, j) in chunk"
-                                :key="j + i"
+                                v-for="(game, i) in genre.games.edges"
+                                :key="i"
                                 class="pt-10"
                             >
                                 <v-card
@@ -43,13 +39,13 @@
                                 >
                                     <v-card-title class="text-center">
                                         <h4>
-                                            {{ game.name }}
+                                            {{ game.node.name }}
                                         </h4>
                                     </v-card-title>
                                     <v-img
                                         height="150"
                                         width="250"
-                                        :src="game.headerImage"
+                                        :src="game.node.headerImage"
                                     ></v-img>
                                 </v-card>
                             </v-col>
@@ -62,47 +58,27 @@
 </template>
 
 <script lang="ts">
-const {Component, VueComponent} = require('@/common/VueComponent');
+const {Component, VueComponent, Prop, Watch} = require('@/common/VueComponent');
 const featuredWinIds = require("../../../../steam/featuredGames.json");
 import axios from "axios";
 
+// TODO: change vue layout to fit updated returned schema.
+// TODO: manipulate query and generate random ids to get more / random genres each page refresh.
 
 @Component
 export default class Games extends VueComponent {
 
+    @Prop({required: true})
     private genreGames = {};
 
-    private async beforeMount(): Promise<void> {
-        await this.loadGames();
+
+
+    @Watch("genreGames")
+    private consoleLog()
+    {
+        // console.log(this.genreGames);
+        this.$forceUpdate();
     }
 
-    private async loadGames(): Promise<void> {
-        this.genreGames = await axios.post("http://127.0.0.1:8000/api/graphql", {
-            query: `
-                query GetGenreWithGamesAndDescription($id: ID!) {
-                    genre(id: $id) {
-                        description
-                        games {
-                            edges {
-                                node {
-                                    id
-                                    name
-                                    headerImage
-                                }
-                            }
-                        }
-                    }
-                }`,
-                variables: {
-                    id: "/api/genres/1",
-                }
-            },
-            {
-                headers: {
-                    'Content-Type': 'application/json'
-                }
-            }
-        );
-    }
 }
 </script>
