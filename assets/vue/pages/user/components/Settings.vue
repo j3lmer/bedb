e9b2cfc5352b0e402ee0a8dc3b36e8b6223641cf
j3lmer <jelmer339@gmail.com>
2022-11-03 13:26:13 +0100

    create card for user settings

--- assets/vue/pages/user/components/Settings.vue
+++ assets/vue/pages/user/components/Settings.vue
@@ -2,10 +2,33 @@
     <v-container>
         <v-row>
             <v-col>
-
+<!--                Als er reviews zijn, anders misschien weergeven dat je nog geen posts hebt-->
+                <h3>Jouw reviews {{isAdminUser ? "en gerapporteerde reviews" : ""}}</h3>
             </v-col>
             <v-col>
+                <v-card>
+                    <v-card-title>
+                        <v-row class="text-center">
+                            <v-col>
+                                <h3>Account instellingen</h3>
+                            </v-col>
+                        </v-row>
+                    </v-card-title>
+                    <v-card-text class="pa-5 pl-10 pb-10">
+                        <v-row class="mb-2 text-center">
+                            <v-col><h4>Gebruikersnaam:</h4></v-col><v-col>{{user.username}} <a><v-icon> mdi-pencil </v-icon></a></v-col>
+                        </v-row>
+                        <v-row class="my-2 text-center">
+                            <v-col><h4>E-mail:</h4></v-col><v-col>{{user.email}}</v-col>
+                        </v-row>
+                        <v-row class="mt-2">
+                            <v-spacer/>
+                            <v-btn>Verander wachtwoord</v-btn>
+                            <v-spacer/>
+                        </v-row>
 
+                    </v-card-text>
+                </v-card>
             </v-col>
         </v-row>
     </v-container>
@@ -25,7 +48,7 @@ export default class Settings extends VueComponent {
     created(): void {
         console.log(this.user);
         this.isAdminUser = !!this.user.roles.includes("ROLE_ADMIN");
-        console.log(this.isAdminUser);
+        //query doen voor alle reviews van
     }
 }
 </script>
